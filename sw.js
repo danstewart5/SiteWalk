d22a71f09d26a1d7ac57cca533254f5d9639ceca
danstewart5 <danstewart5@gmail.com>
2026-09-19 12:43:52 -0700

    Bump cache to sitewalk-v42 for walk modes + voice snap

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'sitewalk-v41';
+const CACHE = 'sitewalk-v42';
 const ASSETS = ['./', './index.html', './walk.js', './clean-speech.js', './manifest.json'];
 
 self.addEventListener('install', (e) => {
