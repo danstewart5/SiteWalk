b96d0bef3f36d570f0c9a37b43772c1f28f889e8
danstewart5 <danstewart5@gmail.com>
2026-09-23 03:44:03 -0700

    Bump cache to sitewalk-v67 so phones pick up walk/report fixes

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'sitewalk-v66';
+const CACHE = 'sitewalk-v67';
 const ASSETS = ['./', './index.html', './walk.js', './clean-speech.js', './voice-value-add.js', './manifest.json'];
 
 self.addEventListener('install', (e) => {
